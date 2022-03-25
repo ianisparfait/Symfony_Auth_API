e65f28e0f72405f07358da1a50b554a494aec843
parfait.ianis <parfait.ianis@gmail.com>
2022-03-26 00:50:22 +0100

    fix login controller

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class DashboardController extends AbstractController
+class LoginController extends AbstractController
 {
     /**
      * @Route("/login", name="app_dashboard")
